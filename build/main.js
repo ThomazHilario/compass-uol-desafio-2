63cb63c3267cc21c5d63510553b79677f9ca981e
Thomaz Alves <thomazhilario5@gmail.com>
2024-08-13 20:12:04 -0300

    feat:happy customers
    - Adicionando animações ao carrousel

--- build/main.js
+++ build/main.js
@@ -12,8 +12,17 @@ const containerCustomers = document.getElementById('container__customers');
 // arrows
 const leftArrowButton = navigationCustomers === null || navigationCustomers === void 0 ? void 0 : navigationCustomers.firstElementChild;
 const rightArrowButton = navigationCustomers === null || navigationCustomers === void 0 ? void 0 : navigationCustomers.lastElementChild;
+let step = 0;
+//containerScrollWidth and containerScrollLeft
+const containerScrollWidth = containerCustomers.scrollWidth;
+const containerScrollLeft = containerCustomers.scrollLeft;
 leftArrowButton.addEventListener('click', () => {
     if (containerCustomers.scrollLeft < containerCustomers.scrollWidth) {
+        step--;
+        // Active animation
+        if (containerCustomers.scrollLeft > 0) {
+            playAnimation('transitionCarrouselBack', '0.2s');
+        }
         containerCustomers.scroll({
             left: containerCustomers.scrollLeft - (containerCustomers.scrollWidth / 5) - 2.65
         });
@@ -21,9 +30,25 @@ leftArrowButton.addEventListener('click', () => {
 });
 rightArrowButton.addEventListener('click', () => {
     if (containerCustomers.scrollLeft >= 0 && containerCustomers.scrollLeft < containerCustomers.scrollWidth) {
+        step++;
+        // Active animation
+        playAnimation('transitionCarrousel', '0.2s');
         containerCustomers.scroll({
             left: containerCustomers.scrollLeft + (containerCustomers.scrollWidth / 5) + 3.65
         });
+        if (step == containerCustomers.children.length && window.innerWidth < 1024) {
+            containerCustomers.scroll({
+                left: 0
+            });
+            step = 0;
+        }
     }
-    console.log(containerCustomers.scrollLeft, containerCustomers.scrollWidth);
 });
+function playAnimation(nameTransition, timeTransition) {
+    containerCustomers.style.animationName = nameTransition;
+    containerCustomers.style.animationDuration = timeTransition;
+    setTimeout(() => {
+        containerCustomers.style.animationName = '';
+        containerCustomers.style.animationDuration = '';
+    }, 400);
+}
--- root/main.ts
+++ root/main.ts
@@ -16,25 +16,61 @@ const containerCustomers = document.getElementById('container__customers') as HT
 const leftArrowButton = navigationCustomers?.firstElementChild as HTMLButtonElement
 const rightArrowButton = navigationCustomers?.lastElementChild as HTMLButtonElement
 
+let step = 0
+
+//containerScrollWidth and containerScrollLeft
+const containerScrollWidth = containerCustomers.scrollWidth
+const containerScrollLeft = containerCustomers.scrollLeft
+
 leftArrowButton.addEventListener('click', () => {
     
     if(containerCustomers.scrollLeft < containerCustomers.scrollWidth){
+        step--
+
+        // Active animation
+        if(containerCustomers.scrollLeft > 0){
+            playAnimation('transitionCarrouselBack','0.2s')
+        }
 
         containerCustomers.scroll(
             {
                 left:containerCustomers.scrollLeft - (containerCustomers.scrollWidth / 5) - 2.65
-            })
+            }
+        )    
     }
-
 })
 
 rightArrowButton.addEventListener('click', () => {
     if(containerCustomers.scrollLeft >= 0 && containerCustomers.scrollLeft < containerCustomers.scrollWidth){
+        step++
+
+        // Active animation
+        playAnimation('transitionCarrousel','0.2s')
+        
 
         containerCustomers.scroll(
             {
                 left: containerCustomers.scrollLeft + (containerCustomers.scrollWidth / 5) + 3.65
-            })
+            }
+        )
+
+        if(step == containerCustomers.children.length && window.innerWidth < 1024){
+            containerCustomers.scroll(
+                {
+                    left: 0
+                }
+            ) 
+            step = 0  
+        }
     }
-    console.log(containerCustomers.scrollLeft, containerCustomers.scrollWidth)
 })
+
+function playAnimation(nameTransition:string, timeTransition:string){
+    containerCustomers.style.animationName = nameTransition
+        containerCustomers.style.animationDuration = timeTransition
+
+    setTimeout(() => {
+        containerCustomers.style.animationName = ''
+        containerCustomers.style.animationDuration = ''
+    },400)
+}
