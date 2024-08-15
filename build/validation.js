90bec1378b54bd6f7f1ddccc473547c05a36eac7
Thomaz Alves <thomazhilario5@gmail.com>
2024-08-15 16:07:16 -0300

    feat: validation.ts
    - Criar estilo de validação para o input daa newsletter.
    - Alterar a validação do input.

--- build/validation.js
+++ build/validation.js
@@ -1,30 +1,42 @@
 "use strict";
+var _a;
 // Form Newsletter
 const formNewsletter = document.querySelector('#newsletter > form');
-// Remove validation default 
-formNewsletter.noValidate = true;
+// get input
+const input = (_a = formNewsletter.firstElementChild) === null || _a === void 0 ? void 0 : _a.lastElementChild;
 // Regex
 const regexForm = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
-formNewsletter.addEventListener('submit', (e) => {
-    var _a;
-    e.preventDefault();
-    // get input
-    const input = (_a = formNewsletter.firstElementChild) === null || _a === void 0 ? void 0 : _a.lastElementChild;
-    verifyValue(input.value, input);
-});
-function verifyValue(valueInput, input) {
-    if (valueInput == '' || !regexForm.test(valueInput)) {
-        const label = input.parentElement;
-        label.style.border = '2px solid red';
-        if (valueInput == '') {
-            return 'Preencha o campo acima';
-        }
+// Required message
+const typeErrors = {
+    inputVazio: 'Preencha este campo!',
+    emailError: 'Email inválido',
+    ok: ''
+};
+// VerifyValue Input
+function verifyValue(valueInput) {
+    // Validation Input
+    if (valueInput !== '') {
+        // Validation Email
         if (!regexForm.test(valueInput)) {
-            return 'Email Invalido';
+            return 'emailError';
         }
     }
     else {
-        const label = input.parentElement;
-        label.style.border = '0';
+        return 'inputVazio';
     }
+    return 'ok';
 }
+// Cancel Refresh page in submit form
+formNewsletter.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+// Input Event 
+input.addEventListener('input', () => {
+    // CustomValidity
+    input.setCustomValidity(typeErrors[verifyValue(input.value)]);
+});
+// Invalid Event
+input.addEventListener('invalid', () => {
+    // CustomValidity
+    input.setCustomValidity(typeErrors[verifyValue(input.value)]);
+});
--- root/validation.ts
+++ root/validation.ts
@@ -1,39 +1,51 @@
 // Form Newsletter
 const formNewsletter = document.querySelector('#newsletter > form') as HTMLFormElement
 
-// Remove validation default 
-formNewsletter.noValidate = true
+// get input
+const input = formNewsletter.firstElementChild?.lastElementChild as HTMLInputElement
 
 // Regex
 const regexForm = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
 
+// Required message
+const typeErrors = {
+    inputVazio: 'Preencha este campo!',
+    emailError:'Email inválido',
+    ok:''
+}
+
+// VerifyValue Input
+function verifyValue(valueInput:string){
+
+    // Validation Input
+    if(valueInput !== ''){
+
+        // Validation Email
+        if(!regexForm.test(valueInput)){
+            return 'emailError'
+        }
+
+    }else{
+        return 'inputVazio' 
+    }
+
+    return 'ok'
+}
+
+// Cancel Refresh page in submit form
 formNewsletter.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    // get input
-    const input = formNewsletter.firstElementChild?.lastElementChild as HTMLInputElement
-
-    
-    verifyValue(input.value, input)
-    
 })
 
-function verifyValue(valueInput:string, input:HTMLInputElement){
+// Input Event 
+input.addEventListener('input', () => {
+    // CustomValidity
+    input.setCustomValidity(typeErrors[verifyValue(input.value) as keyof typeof typeErrors])
+})
 
-    if(valueInput == '' || !regexForm.test(valueInput)){
-        const label = input.parentElement as HTMLLabelElement
-        label.style.border = '2px solid red'
-
-        if(valueInput == ''){
-            return 'Preencha o campo acima'
-        }
-
-        if(!regexForm.test(valueInput)){
-            return 'Email Invalido'
-        }
-    } else{
-        const label = input.parentElement as HTMLLabelElement
-        label.style.border = '0'
-    }
-
-}
+// Invalid Event
+input.addEventListener('invalid', () => {
+    // CustomValidity
+    input.setCustomValidity(typeErrors[verifyValue(input.value) as keyof typeof typeErrors])
+})
